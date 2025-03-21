9e90edd782cc39dcb45badf07d1961070ea83e37
AdamRB208 <adamrborup@gmail.com>
2025-03-21 13:07:20 -0600

    posts in AppState

--- src/services/PostsService.js
+++ src/services/PostsService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { Post } from "@/models/Post.js"
+import { AppState } from "@/AppState.js"
 
 class PostsService {
   async getPosts() {
     const response = await api.get('api/posts')
-    logger.error('got posts!', response.data)
+    logger.log('got posts!', response.data)
+    const posts = response.data.posts.map(pojo => new Post(pojo))
+    AppState.post = posts
   }
 }
 
