614eaef89b5180191fa3a6f590c5d675bd4aab39
AdamRB208 <adamrborup@gmail.com>
2025-03-21 12:54:24 -0600

    got posts

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,49 @@
 <script setup>
-import Example from '@/components/Example.vue';
+import { postsService } from '@/services/PostsService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  getPosts()
+})
+
+async function getPosts() {
+  try {
+    await postsService.getPosts()
+  } catch (error) {
+    Pop.error(error, 'Could not get posts')
+    logger.error('COULD NOT GET POSTS', error)
+  }
+}
 
 
 </script>
 
 <template>
-  <div></div>
+  <section class="container">
+    <div class="row">
+      <div class="col-md-9">
+        <!-- <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">An item</li>
+            <li class="list-group-item">A second item</li>
+            <li class="list-group-item">A third item</li>
+          </ul>
+          <div class="card-body">
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div> -->
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped lang="scss"></style>
